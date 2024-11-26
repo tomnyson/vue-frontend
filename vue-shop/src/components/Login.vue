c39970c1888ff6cbb71e38bc1d14b68e9bd0384e
tomnyson <tabletkindfire@gmail.com>
2024-11-26 07:42:07 +0700

    grantt chart

--- vue-shop/src/components/Login.vue
+++ vue-shop/src/components/Login.vue
@@ -38,22 +38,53 @@
 import { ref, reactive } from "vue";
 import axios from "axios";
 import { API_URL } from "../const.js";
+
 import { useRouter } from 'vue-router'
 const isLogin = ref(false);
 const email = ref("");
 const password = ref("");
 const router = useRouter()
 const handleSubmit = async () => {
-  if (name.value !== "" && password.value !== "") {
-    const data = {
-      email: email.value,
-      password: password.value,
-    };
-    const response = await axios.post(`${API_URL}/login`, data);
-    if (response.status === 200) {
-      localStorage.setItem('token', response.data.token);
-      localStorage.setItem('username', response.data.name);
-      router.push('categories')
+  // if (name.value !== "" && password.value !== "") {
+  //   const data = {
+  //     email: email.value,
+  //     password: password.value,
+  //   };
+  //   const response = await axios.post(`${API_URL}/login`, data);
+  //   if (response.status === 200) {
+  //     localStorage.setItem('token', response.data.token);
+  //     localStorage.setItem('username', response.data.name);
+  //     router.push('categories')
+  //   }
+  // }
+  const payload = {
+    frequency: "daily",
+    date: "2024-11-22T17:00:00.000Z",
+    name: "Đi chợ",
+    value: 10000,
+    createdDate: "2024-11-23T06:04:26.938Z",
+  };
+
+  const instance = axios.create({
+  baseURL: "https://api.bimdev.vn/api/diary",
+  
+
+});
+
+  try {
+    const response = await instance.post(
+      "/expense/list",
+      payload
+    );
+
+    console.log("Response Data:", response.data);
+  } catch (error) {
+    if (error.response) {
+      console.error("API Error:", error.response.data);
+    } else if (error.request) {
+      console.error("No Response Received:", error.request);
+    } else {
+      console.error("Error Setting Up Request:", error.message);
     }
   }
 };
